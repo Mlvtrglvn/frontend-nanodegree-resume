53557850a71b6ff5a5dfe4e6c167e0a2b6efaa51
Dario Bertero <anrvrx@gmail.com>
2015-08-31 20:40:47 +0800

    Turn black empty sections
    Problem set 1 exercise 1

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,40 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+// var str = "Someone is AWESOME!";
+// var newstr = str.replace("AWESOME", "FUN");
+// $("#main").append(newstr);
+
+var formattedName = "Pinco Pallino";
+var formattedRole = "Test role";
+
+HTMLheaderName = HTMLheaderName.replace("%data%", formattedName);
+HTMLheaderRole = HTMLheaderRole.replace("%data%", formattedRole);
+
+$("#main").append(HTMLheaderName);
+$("#main").append(HTMLheaderRole);
+
+var bio = {
+	"name": "My Name",
+	"role": "My role",
+	"contact_info": "contactinf",
+	"picture_Url": "URL",
+	"welcome_message": "Welcome!",
+	"skills": "None"
+};
+
+var work = {
+	"city": "Paperopoli",
+	"position": "Unemployed"
+};
+var education = {
+	"schools": ["Liceo f", "Pt"],
+	"cities": ["Rome", "Turin"],
+	"gradyear": 2012,
+	"majors": "engineering"
+};
+
+var project = {};
+
+$("#main").append(work["position"]);
+$("#main").append(education.name);
